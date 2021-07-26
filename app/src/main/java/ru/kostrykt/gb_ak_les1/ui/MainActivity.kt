31ebba937e2d22d12f80eccbb1c6e0886958a0cb
kos_ykt <kosvasl09@gamil.com>
2021-07-26 16:18:44 +0900

    button.setOnClickListener

--- app/src/main/java/ru/kostrykt/gb_ak_les1/ui/MainActivity.kt
+++ app/src/main/java/ru/kostrykt/gb_ak_les1/ui/MainActivity.kt
@@ -1,10 +1,15 @@
 package ru.kostrykt.gb_ak_les1.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import ru.kostrykt.gb_ak_les1.R
 import ru.kostrykt.gb_ak_les1.repository.RepositoryObject
+
+const val TAG_LOG = "tag_log"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +20,11 @@ class MainActivity : AppCompatActivity() {
         val adapter = WeatherAdapter(RepositoryObject)
 
         recyclerWeather.adapter = adapter
+
+        val button = findViewById<Button>(R.id.my_button)
+        button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажата кнопка", Toast.LENGTH_SHORT).show()
+            Log.i(TAG_LOG, "по кнопке")
+        }
     }
 }
